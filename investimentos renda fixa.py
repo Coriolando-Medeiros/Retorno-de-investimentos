4a51d1b4f789028c7b33c8aca528ae43dd5b4577
Coriolando Medeiros <coriolando.medeiros@gmail.com>
2023-11-12 14:34:33 -0300

    Feito tratamentos de erro na escolha de opções

--- investimentos renda fixa.py
+++ investimentos renda fixa.py
@@ -1,5 +1,7 @@
 # Importa as bibliotecas necessárias
 import os  # Para funções relacionadas ao sistema operacional
+import stdiomask #Para mascarar a senha ao ser digitada
+from string import capwords #Para capitular a primeira letra do usúario
 
 # Define uma função para verificar o login com base no nome de usuário e senha
 def verificar_login(usuario, senha):
@@ -80,29 +82,10 @@ def calcula_final_sem_ir(aporte_inicial, taxa_juros_ano, aporte_mensal, periodo_
     print(f"{usuario} seu investimento de {periodo_meses} meses poderá render R$ {acumulado:.2f} líquidos de IR")
     print(f"Sendo R$ {investimentos} de investimentos e R$ {juros_totais:.2f} de juros")
 
-# Imprime uma mensagem de boas-vindas
-print("Sistema de cálculo de retorno de investimentos")
-
-# Loop principal
-while True:
-    # Solicita o nome de usuário e senha
-    usuario = input("Usuário: ")
-    senha = input("Senha: ")
-    
-    # Verifica o login com base no nome de usuário e senha
-    if verificar_login(usuario, senha):
-        print(f"Login bem-sucedido! {usuario}")
-        break
-    else:
-        print("Usuário ou senha incorretos.")
-    
-
-# Limpa a tela do console
-limpar_tela()
-
-while True:
-    # Exibe as opções do menu para o usuário
+def menu():
+    print("")
     print("Menu")
+    print("")
     print("1 -> Tesouro Direto - IR de 22,5% a 15%")
     print("2 -> CDB - IR de 22,5% a 15%")
     print("3 -> LCI - IR Pessoa Física Isento")
@@ -111,52 +94,84 @@ while True:
     print("6 -> Debêntures - IR de 22,5% a 15%")
     print("7 -> Fundos de Renda Fixa - IR de 22,5% a 15%")
     print("0 -> Sair")
+
+# Imprime uma mensagem de boas-vindas
+print("")
+print("Sistema de cálculo de retorno de investimentos")
+
+# Loop principal
+while True:
+    # Solicita o nome de usuário e senha
+    usuario = input("Usuário: ")
+    senha = stdiomask.getpass(prompt= 'Senha: ', mask='*')
+    nome_capitulado = capwords(usuario)
     
-    # Solicita a opção do usuário e converte para um número inteiro
-    opcao = int(input("Digite a opção: "))
-    
-    # Verifica a opção selecionada pelo usuário
-    if opcao == 0:
-        # Se a opção for 0, exibe agradecimento e sai da aplicação encerrando o loop
-        print("Obrigado por usar nossos sistemas")
-        print("Saindo da aplicação!...")
+    # Verifica o login com base no nome de usuário e senha
+    if verificar_login(usuario, senha):
+        limpar_tela()
+        print(f"Login efetuado! {nome_capitulado}")
         break
+    else:
+        print("Usuário ou senha incorretos.")
     
-    if opcao > 0:
-        # Se a opção for maior que zero, executa o bloco de código dentro do try
-        try:
-            # Tenta converter e obter o valor do investimento inicial, mensal e taxa de juros anual
-            aporte_inicial = float(input("Investimento inicial: ").replace(',', '.'))
-            if aporte_inicial > 0:
-                aporte_mensal = float(input("Investimento mensal: ").replace(',', '.'))
-                if aporte_mensal >= 0:
-                    taxa_juros_ano = float(input("Taxa de juros anual: ").replace(',', '.'))
-                    if taxa_juros_ano >= 0:
-                        periodo_meses = int(input("Tempo de investimentos - meses: "))
-                        # Verifica a opção escolhida e chama a função correspondente
-                        if opcao == 1:
-                            calcula_final_com_ir(aporte_inicial, taxa_juros_ano, aporte_mensal, periodo_meses)
-                        elif opcao == 2:
-                            calcula_final_com_ir(aporte_inicial, taxa_juros_ano, aporte_mensal, periodo_meses)
-                        elif opcao == 3:
-                            calcula_final_sem_ir(aporte_inicial, taxa_juros_ano, aporte_mensal, periodo_meses)
-                        elif opcao == 4:
-                            calcula_final_sem_ir(aporte_inicial, taxa_juros_ano, aporte_mensal, periodo_meses)
-                        elif opcao == 5:
-                            calcula_final_sem_ir(aporte_inicial, taxa_juros_ano, aporte_mensal, periodo_meses)
-                        elif opcao == 6:
-                            calcula_final_com_ir(aporte_inicial, taxa_juros_ano, aporte_mensal, periodo_meses)
-                        elif opcao == 7:
-                            calcula_final_com_ir(aporte_inicial, taxa_juros_ano, aporte_mensal, periodo_meses)
-                    else:
-                        print("Taxa de juros anual inválida!")
-                else:
-                    print("Investimento mensal inválido!")
-            else:
-                print("Investimento inicial inválido!")
-        except ValueError:
-            # Se houver erro na conversão para float, exibe uma mensagem de valor inválido
-            print("Valor inválido. Certifique-se de usar um número válido e utilize ponto (.) em vez de vírgula (,).")
+
+# Limpa a tela do console
+
+
+while True:
+    # Exibe as opções do menu para o usuário
+    menu()
+    # Solicita a opção do usuário e converte para um número inteiro
+    try:
+        opcao = int(input("Digite a opção: "))
+        if opcao > 0 or opcao < 8: 
+                # Verifica a opção selecionada pelo usuário
+                if opcao == 0:
+                    # Se a opção for 0, exibe agradecimento e sai da aplicação encerrando o loop
+                    limpar_tela()
+                    print("Obrigado por usar nossos sistemas")
+                    print("Saindo da aplicação!...")
+                    break
+
+                elif opcao > 0 and opcao < 8:
+                        # Se a opção for maior que zero, executa o bloco de código dentro do try
+                        try:
+                            # Tenta converter e obter o valor do investimento inicial, mensal e taxa de juros anual
+                            aporte_inicial = float(input("Investimento inicial: ").replace(',', '.'))
+                            if aporte_inicial > 0:
+                                aporte_mensal = float(input("Investimento mensal: ").replace(',', '.'))
+                                if aporte_mensal >= 0:
+                                    taxa_juros_ano = float(input("Taxa de juros anual: ").replace(',', '.'))
+                                    if taxa_juros_ano >= 0:
+                                        periodo_meses = int(input("Tempo de investimentos - meses: "))
+                                        # Verifica a opção escolhida e chama a função correspondente
+                                        if opcao == 1:
+                                            calcula_final_com_ir(aporte_inicial, taxa_juros_ano, aporte_mensal, periodo_meses)
+                                        elif opcao == 2:
+                                            calcula_final_com_ir(aporte_inicial, taxa_juros_ano, aporte_mensal, periodo_meses)
+                                        elif opcao == 3:
+                                            calcula_final_sem_ir(aporte_inicial, taxa_juros_ano, aporte_mensal, periodo_meses)
+                                        elif opcao == 4:
+                                            calcula_final_sem_ir(aporte_inicial, taxa_juros_ano, aporte_mensal, periodo_meses)
+                                        elif opcao == 5:
+                                            calcula_final_sem_ir(aporte_inicial, taxa_juros_ano, aporte_mensal, periodo_meses)
+                                        elif opcao == 6:
+                                            calcula_final_com_ir(aporte_inicial, taxa_juros_ano, aporte_mensal, periodo_meses)
+                                        elif opcao == 7:
+                                            calcula_final_com_ir(aporte_inicial, taxa_juros_ano, aporte_mensal, periodo_meses)
+                                    else:
+                                        print("Taxa de juros anual inválida!")
+                                else:
+                                    print("Investimento mensal inválido!")
+                            else:
+                                print("Investimento inicial inválido!")
+                        except ValueError:
+                            # Se houver erro na conversão para float, exibe uma mensagem de valor inválido
+                            print("Valor inválido. Certifique-se de usar um número válido e utilize ponto (.) em vez de vírgula (,).")
+    except ValueError: 
+        # Se houver erro na escolha da opção
+        limpar_tela()
+        print("Valor inválido. Certifique-se de usar um número válido")
     else:
         # Se a opção não for válida, exibe uma mensagem de opção inválida
         print("Opção inválida!")
